87bd46b31d3f7725060de82a5eb71c164259b72c
Alex-dev-if <91799263+Alex-dev-if@users.noreply.github.com>
2023-02-11 12:53:09 -0300

    upload tela de menu

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ImageBackground} from 'react-native';
+
+import * as Animatable from 'react-native-animatable'
+
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function Menu() {
+    const navigation = useNavigation();
+
+    return (
+        <View>
+            <Text>
+            Menu
+            </Text>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    backgroundTema:{
+        
+      },
+      
+      
+    
+})
