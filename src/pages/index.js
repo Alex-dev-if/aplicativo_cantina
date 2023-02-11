c1e59a018906dd6255f355264e949c7be1684dfd
Alex-dev-if <91799263+Alex-dev-if@users.noreply.github.com>
2023-02-11 12:51:46 -0300

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, ImageBackground} from 'react-native';
+
+import * as Animatable from 'react-native-animatable'
+
+import { useNavigation } from '@react-navigation/native';
+
+
+export default function Reserva() {
+    const navigation = useNavigation();
+
+    return (
+        <View>
+            <Text>
+            Reserva
+            </Text>
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    backgroundTema:{
+        
+      },
+      
+      
+    
+})
