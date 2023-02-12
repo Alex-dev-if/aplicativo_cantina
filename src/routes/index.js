f1b13bc2311ada6f87f71371a657481e73068f56
MarcelinoS34 <113615970+MarcelinoS34@users.noreply.github.com>
2023-02-12 15:21:12 -0300

    Mais nomes de telas adicionadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ import Welcome from '../pages/Welcome'
 import SignIn from '../pages/SignIn'
 import TelaInicio from '../pages/TelaInicio'
 import Cadastro from '../pages/Cadastro'
+import Menu from '../pages/Menu'
+import Reserva from '../pages/Reserva'
 
 const Stack = createNativeStackNavigator();
 
@@ -31,6 +33,18 @@ export default function Routes(){
             <Stack.Screen
                 name="Cadastro"
                 component={Cadastro}
+                options={{ headerShown: false}}
+            />
+
+            <Stack.Screen
+                name="Menu"
+                component={Menu}
+                options={{ headerShown: false}}
+            />
+
+            <Stack.Screen
+                name="Reserva"
+                component={Reserva}
                 options={{ headerShown: false}}
             />
         </Stack.Navigator>
